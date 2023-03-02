1b877ec1fb17cee43bee2fdae5dfc99e7a0494e3
umsungjun <99730910+umsungjun@users.noreply.github.com>
2023-03-02 15:27:45 +0900

    08_테스트 로직 만들기

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,26 +1,76 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 import { QuestionData } from '../stores/Question/QuestionData'; //더미데이터 import
 import Header from '../components/Header';
 
 export default function QuestionPage(): React.ReactElement {
   const [qeustionNo, setQeustionNo] = useState(0); // 문제 번호 state
-  const handleClickAnswer = () => {
-    setQeustionNo(qeustionNo + 1);
+  const [totalScore, setTotalScore] = useState([
+    { id: 'EI', score: 0 },
+    { id: 'SN', score: 0 },
+    { id: 'TF', score: 0 },
+    { id: 'JP', score: 0 },
+  ]);
+  const navigate = useNavigate();
+
+  const HandleClickAnswer = (ans: number, type: string) => {
+    const newScore = totalScore.map(s =>
+      s.id === type ? { id: s.id, score: s.score + ans } : s,
+    );
+
+    setTotalScore(newScore);
+    // 마지막 문제가 아닐 때
+    if (QuestionData.length !== qeustionNo + 1) {
+      setQeustionNo(qeustionNo + 1);
+    } else {
+      navigate('/result');
+    }
+    // if (type === 'EI') {
+    //   // 기존스코어에 대한 새로운 스코어 값을 계산
+    //   const addScore = totalScore[0].score + ans;
+    //   // 새로운 스코어를 반영한 새로운 객체
+    //   const object = { id: 'EI', score: addScore };
+    //   // 새로운 객체를 기존의 totalScore배열에 반영
+    //   totalScore.splice(0, 1, object);
+    // } else if (type === 'SN') {
+    //   const addScore = totalScore[1].score + ans;
+    //   const object = { id: 'EI', score: addScore };
+    //   totalScore.splice(0, 1, object);
+    // } else if (type === 'TF') {
+    //   const addScore = totalScore[1].score + ans;
+    //   const object = { id: 'EI', score: addScore };
+    //   totalScore.splice(0, 1, object);
+    // } else if (type === 'JP') {
+    //   const addScore = totalScore[1].score + ans;
+    //   const object = { id: 'EI', score: addScore };
+    //   totalScore.splice(0, 1, object);
+    // }
   };
 
+  useEffect(() => {
+    console.log(totalScore);
+  }, [totalScore]);
   return (
     <Wrapper>
       <Header type="progress" questionNo={qeustionNo} />
       <ContentWrapper>
         <Title>{QuestionData[qeustionNo].title}</Title>
         <ButtonGroup>
-          <Button variant="primary" size="lg" onClick={handleClickAnswer}>
+          <Button
+            variant="primary"
+            size="lg"
+            onClick={() => HandleClickAnswer(1, QuestionData[qeustionNo].type)}
+          >
             {QuestionData[qeustionNo].answera}
           </Button>
-          <Button variant="danger" size="lg" onClick={handleClickAnswer}>
+          <Button
+            variant="danger"
+            size="lg"
+            onClick={() => HandleClickAnswer(0, QuestionData[qeustionNo].type)}
+          >
             {QuestionData[qeustionNo].answerb}
           </Button>
         </ButtonGroup>
